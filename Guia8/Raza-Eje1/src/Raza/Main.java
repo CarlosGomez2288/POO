f08b9816f193204fa3ad63b6f8930a30d9e24e06
CarlosGomez2288 <cargome409@gmail.com>
2022-07-19 10:52:59 -0300

    agregando collecion

---
+++ Guia8/Raza-Eje1/src/Raza/Main.java
@@ -0,0 +1,12 @@
+package Raza;
+
+import Service.ServiRaza;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ServiRaza sr = new ServiRaza();
+        sr.Crear();
+        sr.Mostrar();
+    }
+}
